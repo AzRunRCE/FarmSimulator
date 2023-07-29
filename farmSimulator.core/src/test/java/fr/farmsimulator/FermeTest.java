81bec28dfb2bc46c5c5c86a83cdbd93a1f8f6c0f
Antonin-lhp5 <totemdebois@gmail.com>
2023-07-30 00:17:11 +0200

    Test sur solde de pieces (100)

--- farmSimulator.core/src/test/java/fr/farmsimulator/FermeTest.java
+++ farmSimulator.core/src/test/java/fr/farmsimulator/FermeTest.java
@@ -6,18 +6,17 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
 public class FermeTest {
-
     @Test
     public void constructorFermeTest () {
-        Ferme maFerme = new Ferme();
+        Ferme maFerme = new Ferme(100);
         assertNotNull(maFerme);
     }
 
     @Test
     public void initialisationFermeTest () {
-        Ferme maFerme = new Ferme();
+        Ferme maFerme = new Ferme(100);
         assertNotNull(maFerme);
-
+        assertEquals(100, maFerme.pieces);
         assertEquals(1, maFerme.poules.size());
     }
 
