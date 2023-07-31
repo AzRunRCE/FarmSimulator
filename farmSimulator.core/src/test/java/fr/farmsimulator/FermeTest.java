80f36cd2d2979b1695de17d4c5f351435bf855f5
Antonin-lhp5 <totemdebois@gmail.com>
2023-07-31 17:07:41 +0200

    Affich le nombre de poule et les noms de la liste des poules

--- farmSimulator.core/src/test/java/fr/farmsimulator/FermeTest.java
+++ farmSimulator.core/src/test/java/fr/farmsimulator/FermeTest.java
@@ -15,9 +15,17 @@ public class FermeTest {
     @Test
     public void initialisationFermeTest () {
         Ferme maFerme = new Ferme(100);
+
         assertNotNull(maFerme);
         assertEquals(100, maFerme.pieces);
         assertEquals(1, maFerme.poules.size());
+
+        System.out.println("Nombre de poule : "+ maFerme.poules.size());
+
+        // Affiche les noms de la liste des poules
+        for (Poule poule : maFerme.poules) {
+            System.out.println("Nom : " + poule.getNom());
+        }
     }
 
 }
