f31306e9193f7498f5ded2f6e16acf46e1854512
Antonin-lhp5 <totemdebois@gmail.com>
2023-10-22 23:57:01 +0200

    Nouveau test avec configuration pour init une ferme et suppression de tests

--- farmSimulator.core/src/test/java/fr/farmsimulator/FermeTest.java
+++ farmSimulator.core/src/test/java/fr/farmsimulator/FermeTest.java
@@ -1,15 +1,10 @@
 package fr.farmsimulator;
-
-import fr.farmsimulator.utils.NomsIndispo;
+import fr.farmsimulator.beans.Ferme;
+import fr.farmsimulator.config.ConfigurationFerme;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static fr.farmsimulator.Production.productionOeufs;
-import static fr.farmsimulator.utils.NomAleatoire.AjouterNom;
-import static fr.farmsimulator.utils.NomsIndispo.ListNomsIndisponibles;
-import static junit.framework.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class FermeTest {
 
@@ -19,92 +14,27 @@ public class FermeTest {
     //TODO: GetPonteTotalJour qui fais appel a chacune de tes poules de ton poulalier et qui somme le tous.
     @Test
     public void constructorFermeTest() {
-        Ferme maFerme = new Ferme(100);
+        ConfigurationFerme config = new ConfigurationFerme(150, 2);
+        Ferme maFerme = new Ferme(config);
         assertNotNull(maFerme);
     }
 
     @Test
     public void initialisationFermeTest() {
-        Ferme maFerme = new Ferme(100);
+
+        int piece = 100;
+        int nombrePoule = 5;
+
+        ConfigurationFerme config = new ConfigurationFerme(piece, nombrePoule);
+        Ferme maFerme = new Ferme(config);
 
         assertNotNull(maFerme);
-        assertEquals(100, maFerme.pieces);
-        assertEquals(1, maFerme.poules.size());
+        assertEquals(piece, maFerme.pieces);
+        assertEquals(nombrePoule, maFerme.poules.size());
 
         System.out.println("Nombre de poule : " + maFerme.poules.size());
 
-        /*
-        Affiche les noms de la liste des poules
-        for (Poule poule : maFerme.poules) {
-            System.out.println("Nom : " + poule.getNom());
-        }
-        */
     }
 
-    @Test
-    public void productionOeufAucunePouleTest() {
-        List<Poule> poules = new ArrayList<>();
-        int totalDesOeufs = Production.productionOeufs(poules);
 
-        // Aucune poule dans la list
-        assertEquals(0, totalDesOeufs);
-
-        //System.out.println("Sur " + poules.size() + " poule, vous avez eu " + totalDesOeufs + " oeufs");
-    }
-
-    @Test
-    public void productionOeufUnePouleTest() {
-        Ferme maFerme = new Ferme(100);
-
-        int nombreDePoules = maFerme.poules.size();
-        int totalDesOeufs = productionOeufs(maFerme.poules);
-
-        assertTrue("Le total des oeufs doit être supérieur ou égale à 0",
-                totalDesOeufs >= 0);
-        assertTrue("Le total des oeufs doit être inférieur et égale à 1 poule multiplié par 2",
-                totalDesOeufs <= nombreDePoules * 2);
-
-        //System.out.println("Sur " + nombreDePoules + " poule, vous avez eu " + totalDesOeufs + " oeuf(s)");
-    }
-
-    @Test
-    public void productionOeufDeuxPoulesTest() {
-        Ferme maFerme = new Ferme(100);
-
-        // Ajout d'une nouvelle poule
-        maFerme.poules.add(new Poule("Gérard"));
-
-        int nombreDePoules = maFerme.poules.size();
-        int totalDesOeufs = productionOeufs(maFerme.poules);
-
-        assertTrue("Le total des oeufs doit être supérieur ou égale à 0",
-                totalDesOeufs >= 0);
-        assertTrue("Le total des oeufs doit être inférieur et égale 2 poules multiplié par 2",
-                totalDesOeufs <= nombreDePoules * 2);
-
-        //System.out.println("Sur " + nombreDePoules + " poules, vous avez eu " + totalDesOeufs + " oeuf(s)");
-
-    }
-
-    @Test
-    public void AcheterPouleActionTest() {
-        Ferme maFerme = new Ferme(1000);
-
-        List<String> ListNomsIndisponibles = ListNomsIndisponibles(maFerme);
-
-        Poule nouvellePoule = new Poule(AjouterNom(ListNomsIndisponibles));
-
-        new AcheterPouleAction(maFerme, nouvellePoule);
-
-        assertTrue("Le nombre de poule de la ferme est supérieur à 2",
-                maFerme.poules.size() > 1);
-//        assertEquals(85, maFerme.pieces);
-
-        System.out.println(maFerme.pieces);
-
-        for (Poule poule : maFerme.poules) {
-            System.out.println("Nom : " + poule.getNom());
-        }
-
-    }
 }
