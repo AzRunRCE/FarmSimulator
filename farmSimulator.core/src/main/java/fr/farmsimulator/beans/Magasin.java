13d0b4343a0d0bd6ccc74e6a07457ecbf2181a78
Antonin-lhp5 <totemdebois@gmail.com>
2023-10-22 23:58:59 +0200

    Création de la class Magasin

---
+++ farmSimulator.core/src/main/java/fr/farmsimulator/beans/Magasin.java
@@ -0,0 +1,17 @@
+package fr.farmsimulator.beans;
+
+public class Magasin {
+    private int prixPoule;
+
+    public Magasin() {
+        this.prixPoule = 15;
+    }
+
+    public int getPrixPoule() {
+        return prixPoule;
+    }
+
+    public void setPrixPoule(int prixPoule) {
+        this.prixPoule = prixPoule;
+    }
+}
